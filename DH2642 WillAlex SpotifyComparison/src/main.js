6f65e7c052add55f9e6901273e1423a7776239c6
William Rolf <williamrolf@yahoo.se>
2022-12-07 15:42:20 +0100

    Attempting commit

---
+++ DH2642 WillAlex SpotifyComparison/src/main.js
@@ -0,0 +1,4 @@
+import { render } from "vue";
+import VueRoot from "./presenters/VueRoot.js";
+
+render(<VueRoot />, document.getElementById("app"));
---
+++ DH2642 WillAlex SpotifyComparison/src/presenters/VueRoot.js
@@ -0,0 +1,13 @@
+import test from "../views/test.js";
+
+const VueRoot = {
+  render() {
+    return (
+      <div>
+        <test name="Axel"></test>
+      </div>
+    );
+  },
+};
+
+export default VueRoot;
